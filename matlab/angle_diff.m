20af76a7f8bb39147c2b7b82c05e86061cdc98f5
bfotheri <fotheringhambrett@gmail.com>
2020-04-23 15:00:24 -0600

    removed packages we're not using righ tnow

--- matlab/angle_diff.m
+++
@@ -1,9 +0,0 @@
-function [d] = angle_diff(a, b)
-    % Computes a-b, preserving the correct sign (counter-clockwise positive angles)
-    % All angles are in degrees
-    a = mod(360000 + a, 360);
-    b = mod(360000 + b, 360);
-    d = a - b;
-    d = mod(d + 180, 360) - 180;
-end
-
--- matlab/frontierDetection.m
+++
@@ -1,5 +0,0 @@
-function [frontGrid] = frontierDetection(occGrid)
-kernel = [1, 1, 1; 1, -81, 1; 1, 1, 1];
-frontGrid = conv2(occGrid, kernel, 'same')>=9;
-end
-
